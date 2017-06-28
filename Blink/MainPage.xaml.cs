a4f592aae44c17a5e194bfaa998ed8cef9b86f60
DanielMeixner <daniel.meixner@live.com>
2017-06-28 15:16:18 +0200

    101 - 5 sec

--- Blink/MainPage.xaml.cs
+++ Blink/MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace Blink
             this.InitializeComponent();
             InitGPIO();
             mainTimer = new DispatcherTimer();
-            mainTimer.Interval = new TimeSpan(0, 0, 1);
+            mainTimer.Interval = new TimeSpan(0, 0, 5);
             mainTimer.Tick += MainTimer_Tick;
             mainTimer.Start(); 
         }
