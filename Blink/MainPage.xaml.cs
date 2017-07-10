5544ed7a4bb91999321cda458b58417aa2601c27
DanielMeixner <daniel.meixner@live.com>
2017-07-10 10:56:37 +0200

    removed nuget for iot client, switched to red

--- Blink/MainPage.xaml.cs
+++ Blink/MainPage.xaml.cs
@@ -1,23 +1,8 @@
-using Microsoft.Azure.Devices.Client;
-using Microsoft.Azure.Devices.Shared;
-
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Devices.Gpio;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,7 +22,7 @@ namespace Blink
         }
 
         // change color here
-        private const int LED_PIN = (int)LedColors.Orange;
+        private const int LED_PIN = (int)LedColors.Red;
 
         private int LEDStatus = 0;
         private GpioPin pin;
