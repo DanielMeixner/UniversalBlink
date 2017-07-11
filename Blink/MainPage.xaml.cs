1aa16a66e25b0322d513df64cca16dcb857f6188
DanielMeixner <daniel.meixner@live.com>
2017-07-11 13:27:22 +0200

    changed color to Orange

--- Blink/MainPage.xaml.cs
+++ Blink/MainPage.xaml.cs
@@ -22,7 +22,7 @@ namespace Blink
         }
 
         // change color here
-        private const int LED_PIN = (int)LedColors.Green;
+        private const int LED_PIN = (int)LedColors.Orange;
 
         private int LEDStatus = 0;
         private GpioPin pin;
